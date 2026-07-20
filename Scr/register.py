d4c74f52c1fdffb218d6d0254d8b47d8812536e2
Sanjana <sanjana.sureshkumar05@gmail.com>
2026-07-20 09:09:41 +0530

    Deleted register.py

--- Scr/register.py
+++
@@ -1,8 +0,0 @@
-print("User Registration \n")
-username = input("Enter username :")
-email = input("Enter email ID :")
-password = input("Enter password: ")
-number=input("Enter Mobile number: ")
-
-print("User Added")
-
